43a1040d55a4db7cc3a07d581317936776c3d3bb
esyamamoto <erika.s.yamamoto@gmail.com>
2024-05-16 17:26:41 -0300

    PreAuthorize no controller crop e fertilizer

--- src/main/java/com/betrybe/agrix/ebytr/staff/controller/FertilizerController.java
+++ src/main/java/com/betrybe/agrix/ebytr/staff/controller/FertilizerController.java
@@ -8,6 +8,7 @@ import com.betrybe.agrix.ebytr.staff.service.FertilizerService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -45,6 +46,7 @@ public class FertilizerController {
    * GETALL.
    */
   @GetMapping
+  @PreAuthorize("hasAnyRole('ROLE_ADMIN')")
   public List<FertilizerDto> getAllCrops() {
     List<Fertilizer> allCrops = fertilizerService.findAll();
     return allCrops.stream()
